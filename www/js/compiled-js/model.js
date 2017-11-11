85e1d19c4184070a8d00fa80469a1b5c64b1b522
Osahon Okungbowa <writeosahon@yahoo.co.uk>
2017-11-11 22:50:27 +0100

    initial commit - design and implementation

--- www/js/compiled-js/model.js
+++ www/js/compiled-js/model.js
@@ -28,8 +28,20 @@ utopiasoftware.heritage.model = {
 // register the event listener for when all Hybrid plugins and document DOM are ready
 document.addEventListener("app.Ready", utopiasoftware.heritage.controller.appReady, false) ;
 
+// listen for the initialisation of the Onboarding page
+$(document).on("init", "#onboarding-page", utopiasoftware.heritage.controller.onboardingPageViewModel.pageInit);
+
+// listen for when the Onboarding page is shown
+$(document).on("show", "#onboarding-page", utopiasoftware.heritage.controller.onboardingPageViewModel.pageShow);
+
+// listen for when the Onboarding page is hidden
+$(document).on("hide", "#onboarding-page", utopiasoftware.heritage.controller.onboardingPageViewModel.pageHide);
+
+// used to listen for 'onboarding-carousel' carousel items/slide changes
+$(document).on("postchange", "#onboarding-carousel", utopiasoftware.heritage.controller.onboardingPageViewModel.carouselPostChange);
+
 // listen for the initialisation of the Main-Menu page
-$(document).on("init", "#main-menu-page", utopiasoftware.heritage.controller.mainMenuPageViewModel.pageInit);
+$(document).on("init", "#main-menu-page", utopiasoftware.heritage.controller.onboardingPageViewModel.pageInit);
 
 // listen for when the Main-Menu page is shown
 $(document).on("show", "#main-menu-page", utopiasoftware.heritage.controller.mainMenuPageViewModel.pageShow);
