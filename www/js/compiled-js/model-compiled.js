b1ccc34e1d699702af4d2f38597a470869024742
Osahon Okungbowa <writeosahon@yahoo.co.uk>
2018-02-06 09:01:20 +0100

    1st commit - design and implementation

--- www/js/compiled-js/model-compiled.js
+++ www/js/compiled-js/model-compiled.js
@@ -28,8 +28,7 @@ utopiasoftware.ally._model = {
     /**
      * property is used to hold the base url for communicating with ALLY app server
      */
-    ally_base_url: "https://myallyapp.com/test", //"https://myallyapp.com",
-
+    ally_base_url: "https://myallyapp.com",
 
     /**
      * property holds the google app share/dynamic link for this app
